0796dd52d2f0fc14b27868e190a5f95fb193906b
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-13 10:34:52 -0700

    add park detail component for user to retrieve details on park when clicked from list
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-Authored-by: Dominique Youmans <ddyoumans@icloud.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

---
+++ src/components/ParkDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ParkDetails(props) {
+  const { park, onClickingDelete } = props; 
+
+  return (
+    <React.Fragment>
+      <h2><strong>{park.name}</strong></h2>
+      <h4>{park.city}, {park.state}</h4>
+      <h5>Enjoy climbimg <strong>{park.climbingRoutes} </strong> routes in our majestic mountains or on our boulders!</h5>
+      <h5>Need a place to sleep? We have {park.campGrounds} campgrounds to choose from ranging from water front properties to mountainous.</h5>
+      <h5>If you need to grab anything we have {park.generalStores} general store(s) to make your stay in {park.name} more comfortable.</h5>
+      <button onClick={ props.onClickingEdit }>Update Park</button>
+    </React.Fragment>
+  );
+}
+
+ParkDetail.propTypes = {
+  park: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default ParkDetail;

616842a033f6dbb47e85e19e49fce6f689b0428d
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-13 09:48:27 -0700

    create park component and park list
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-Authored-by: Dominique Youmans <ddyoumans@icloud.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- src/components/Park.js
+++ src/components/Park.js
@@ -7,7 +7,11 @@ function Park(props) {
   return (
     <React.Fragment>
       <div onClick = {() => props.whenParkClicked(props.id)}>
-        
+      <h2>{props.name}</h2>
+      <h4>{props.city}, {props.state}</h4>
+      <h5>Climbing Routes: {props.climbingRoutes}</h5>
+      <h5>Campgrounds: {props.campgrounds}</h5>
+      <h5>General Stores: {props.generalStores}</h5>
       </div>
     </React.Fragment>
   )
@@ -15,8 +19,15 @@ function Park(props) {
 
 
 Park.propTypes = {
-    
-}
+    name: PropTypes.string,
+    city: PropTypes.string,
+    state: PropTypes.string,
+    climbingRoutes: PropTypes.number,
+    campgrounds: PropTypes.number,
+    generalStores: PropTypes.number,
+    whenParkClicked: PropTypes.func
+
+};
 
 
 export default Park;
---
+++ src/components/ParkList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Park from './Park';
+import PropTypes from 'prop-types';
+
+function ParkList(props) {
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.parkList.map((park) => 
+      <Park
+        whenParkClicked = { props.onParkSelection }
+        name={park.name}
+        city={park.city}
+        state={park.state}
+        climbingRouts={park.climbingRouts}
+        campgrounds={park.campgrounds}
+        generalStores={park.generalStores}
+        id={park.id}
+        key={park.id}
+        />
+      )}
+    </React.Fragment>
+  );
+}
+
+ParkList.propTypes = {
+  parkList: PropTypes.array,
+  onParkSelection: PropTypes.func
+};
+
+export default ParkList;

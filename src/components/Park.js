77c61ef4c19fb8eb9d6f94657968ebc53b58e05e
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-13 13:20:35 -0700

    refactor park component and add reusable form, editing and delete func
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-Authored-by: Dominique Youmans <ddyoumans@icloud.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- src/components/Park.js
+++ src/components/Park.js
@@ -1,9 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, {useState} from 'react';
+// import PropTypes from "prop-types";
+import ReusableForm from './ReusableForm';
 
+export default function Park(props) {
+  const { park } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  let button = isEditing ? null : <button onClick = {() => setIsEditing(!isEditing)}>Edit Park</button>
 
-function Park(props) {
+  const handleEditParkFormSubmission = event => {
+    event.preventDefault();
+    const propertiesToUpdate ={
+      name: event.target.name.value,
+      climbingRoutes: event.target.climbingRoutes.value,
+      camgrounds: event.target.camgrounds.value,
+      generalStores: event.target.generalStores.value
+    }
 
+    props.onEditPark(park.parkId, propertiesToUpdate);
+    setIsEditing(!isEditing);
+  }
 
   return (
     <React.Fragment>
@@ -13,22 +28,26 @@ function Park(props) {
       <h5>Climbing Routes: {props.climbingRoutes}</h5>
       <h5>Campgrounds: {props.campgrounds}</h5>
       <h5>General Stores: {props.generalStores}</h5>
+      {button}
+      {isEditing ? <ReusableForm formSubmissionHandler={handleEditParkFormSubmission} 
+        buttonText="Save Park Changes!" /> : null}}
+      <button onClick={() => props.onDeletePark(park.parkId)}>Delete Park</button>
       </div>
     </React.Fragment>
   )
 }
 
 
-Park.propTypes = {
-    name: PropTypes.string,
-    city: PropTypes.string,
-    state: PropTypes.string,
-    climbingRoutes: PropTypes.number,
-    campgrounds: PropTypes.number,
-    generalStores: PropTypes.number,
-    whenParkClicked: PropTypes.func
+// Park.propTypes = {
+//     name: PropTypes.string,
+//     city: PropTypes.string,
+//     state: PropTypes.string,
+//     climbingRoutes: PropTypes.number,
+//     campgrounds: PropTypes.number,
+//     generalStores: PropTypes.number,
+//     whenParkClicked: PropTypes.func
 
-};
+// };
 
 
-export default Park;
+// export default Park;

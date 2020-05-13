de22fe89deafd28891f822ca1f72f30cbcc244d7
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-13 09:26:23 -0700

    start park component and action type constants
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- src/components/Park.js
+++ src/components/Park.js
@@ -2,5 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Park(props) {
-  
+
+
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenParkClicked(props.id)}>
+        
+      </div>
+    </React.Fragment>
+  )
 }
+
+
+Park.propTypes = {
+    
+}
+
+
+export default Park;
